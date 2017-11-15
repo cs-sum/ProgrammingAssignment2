051328f765db80bd6d001a1a0f4a0227df80784f
cs-sum <33584061+cs-sum@users.noreply.github.com>
2017-11-14 22:50:18 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix is a list containing three functions used to cache the normal matrix
+## and inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+	nmat <- x
+	imatx <- NULL
 
+      getn <- function() nmat
+      setinv <- function(ix) imatx <<- ix
+      getinv <- function() imatx
+      list(	getn = getn,
+		setinv = setinv,
+           	getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function to calculate the inverse matrix from a list created using
+## above makeCacheMatrix or to return the cached matrix from the list
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+	m <- x$getinv()
+	if (!is.null(m)) {
+		message("getting inverse matrix from cache")
+		return(m)
+	}
+	data <- x$getn()
+	inv <- solve(data)
+	x$setinv(inv)
+	inv
 }
